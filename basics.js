a5174e4c835cfe2ccbfe3ed4f5c3f8d8792a6ce4
clau9156 <clau9156@stud.kea.dk>
2021-02-06 16:21:20 +0100

    typeof showing what it means

---
+++ basics.js
@@ -0,0 +1,23 @@
+// console.log("hi");
+"use strict";
+
+// y = 14;
+
+const bool = true;
+const num = 41;
+const str = "Peter";
+const obj = {
+    cats: 2, 
+    cars: 1
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+// typeof bool;
+// typeof num;
+// typeof str;
+// typeof obj;
+// typeof notthing;
+// typeof undf;
+// typeof symbol;
