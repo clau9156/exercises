b5b4e0a776d5770cf3ae63ef91abaa999a71f5aa
clau9156 <clau9156@stud.kea.dk>
2021-02-08 17:52:01 +0100

    exercise done

---
+++ 3caPitalization/capitalization3.js
@@ -0,0 +1,15 @@
+ "use strict"; 
+
+const name = "peter";
+
+const firstPart = name.substring(0,2).toLowerCase();
+const thirdLetter = name.substring(2,3).toUpperCase()
+const lastPart = name.substring(3,5).toLowerCase();
+console.log(firstPart+thirdLetter+lastPart); 
+
+
+// const name = "peter";
+
+const firstLetter = name.substring(0,1).toUpperCase()
+const lastLetter = name.substring(1,5).toLowerCase();
+console.log(firstLetter+lastLetter); 
