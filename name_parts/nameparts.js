d473b52f9d3871cd562eddce7098ce57ef877b2e
clau9156 <clau9156@stud.kea.dk>
2021-02-08 11:52:34 +0100

    now files added with change

---
+++ name_parts/nameparts.js
@@ -0,0 +1,22 @@
+// const name = "Peter Heronimous Lind";
+// let first = name.split(" ")[0];
+// console.log(first);
+// let middle = name.split(" ")[1];
+// console.log(middle);
+// let last = name.split(" ")[2];
+// console.log(last);
+
+// first = first[0];
+// middle = middle[1];
+// last = last[2];
+
+let fullName = "Peter Heronimous Lind".split(' ');
+firstName = fullName[0];
+middleName = fullName[1];
+lastName = fullName[2];
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
+
+
+// const name = "Peter Heronimous Lind";
