e367369fafe1c15447c3cdcb14253ae16b289cfb
clau9156 <clau9156@stud.kea.dk>
2021-02-11 11:55:18 +0100

    test file for loop

---
+++ test/script.js
@@ -0,0 +1,26 @@
+    // 0-9
+// for (let bs=0; bs<10; bs++) {
+//     console.log(bs);
+// }
+
+    // 1-10
+// for (let bs=1; bs<11; bs++) {
+//     console.log(bs);
+// }
+
+    // 10-0
+// for (let bs=10; bs>=0; bs--) {
+//     console.log(bs);
+// }
+
+    // 1-19 odd numbers
+// for (let bs=1; bs<18;) {
+//     bs+=2;
+//     console.log(bs);
+// }
+
+    // 1-16777216 doubling
+
+    // 111-138 +3
+
+    // 100-0 -10
